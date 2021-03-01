b45740ba52e50ca9ad836881bb8091d078ac3a1b
于海洋 <yuhaiyang0427@gmail.com>
2021-03-01 14:10:20 +0800

    服务器项目初始化

---
+++ app.js
@@ -0,0 +1,35 @@
+const fs = require('fs')
+const express = require('express')
+const mongoose = require('mongoose')
+const Picture = require('./models/picture')
+const app = express()
+
+//解析请求体中传递的rul编码的数据
+app.use(express.urlencoded()) //使用中间件
+app.use(express.json())
+
+app.use('/api/v1/p',require('./routes/v1/picture'))
+
+mongoose.connect('mongodb://localhost:27017')
+  .then(()=>{
+    console.log('数据库连接成功')
+    // loadData()
+    deleteManyData()
+  })
+  .catch(err=>console.log(err))
+
+// function loadData(){
+//   const dataList = JSON.parse(fs.readFileSync('./static/skillsList.json'))
+//   console.log(dataList);
+//   Picture.insertMany(dataList).then(()=>console.log('插入数据成功'))
+// }
+
+
+// 删除多条数据
+  function deleteManyData(){
+    const dataList = JSON.parse(fs.readFileSync('./static/skillsList.json'))
+    Picture.deleteMany({image: {$in: dataList}}).then(()=>console.log('删除成功'))
+  }
+app.listen(8088,() => {
+  console.log('服务器启动在8088端口。。。')
+})
